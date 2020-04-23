f77ad1614c24b7f7c12dbcfde713d15c64833ba4
MaryHooker <mc020791@yahoo.com>
2020-04-23 11:07:20 -0500

    simple grid

--- client/src/components/AppContainer.jsx
+++ client/src/components/AppContainer.jsx
@@ -1,23 +1,86 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import AddAccount from './AddAccount';
 import ListAccounts from './ListAccounts';
-import ViewAccount from './ViewAccount';
+// import ViewAccount from './ViewAccount';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 class AppContainer extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            addAccountForm: false,
+            listAllAccounts: false,
+        }
     }
-    render() { 
-        return ( 
+
+    //when add account button is clicked/display form
+    addAccount = () => {
+        this.setState(
+            {
+                addAccountForm: true
+            }
+        )
+        //hide list
+        this.setState(
+            {
+                listAllAccounts: false
+            }
+        )
+    }
+
+    //when available accounts button is clicked/display all accounts
+    showAccounts = () => {
+        this.setState(
+            {
+                listAllAccounts: true
+            }
+        )
+        //hide form
+        this.setState(
+            {
+                addAccountForm: false
+            }
+        )
+    }
+
+    render() {
+        let displayForm;
+        let displayAccounts;
+
+        //conditionally rendering account form 
+        if (this.state.addAccountForm) {
+            displayForm = <AddAccount />
+        }
+
+        //conditionally rendering account list
+        if (this.state.listAllAccounts) {
+            displayAccounts = <ListAccounts />
+        }
+        return (
             <div>
-                <h1>Bank Account Manager</h1>
-                <AddAccount/>
-                <ListAccounts/>
-                <ViewAccount/>
-            </div>
-         );
+                <Router>
+                    <Link to='/'>Home</Link>
+                    <h1>Bank Account Manager</h1>
+                    <div className='appContainer'>
+                    <div>
+                        <button onClick={this.addAccount}>Add Bank Account</button>
+                    </div>
+                    <div>
+                        <button onClick={this.showAccounts}>Available Accounts</button>
+                    </div>
+                    <div>
+                        {displayForm}
+                    </div>
+                    <div>
+                        {displayAccounts}
+                    </div>
+                    </div>
+                </Router>
+           
+                {/* <ViewAccount/> */ }
+            </div >
+        );
     }
 }
- 
+
 export default AppContainer;
