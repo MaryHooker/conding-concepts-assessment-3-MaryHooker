d1834fc1e0e40122bd28152f63715107fba169f3
MaryHooker <mc020791@yahoo.com>
2020-04-23 10:25:15 -0500

    endpoints tested

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,10 +7,42 @@ router.use(express.json());
 //import model
 let BankAccountCollection = require('../models/BankAccountSchema');
 
-//sanity
-router.get('/test',(req,res)=>{
-    res.send(`Loud and Clear`);
+
+//Create new bank account
+router.post('/',(req,res)=>{
+    console.log(`Create request!`)
+    res.send(`Account created!`);
 })
+
+//Read on bank account by account number
+router.get('/:accountNumber',(req,res)=>{
+    console.log(`Get request of ${req.params.accountNumber}!`)
+    res.send(`Read specific account!`);
+})
+
+//Update bank account balance
+router.put('/:accountBalance',(req,res)=>{
+    console.log(`Update request of ${req.params.accountBalance}!`)
+    res.send(`Account updated!`);
+})
+
+//Delete bank account balance
+router.delete('/:accountBalance',(req,res)=>{
+    console.log(`Delete request of ${req.params.accountBalance}!`)
+    res.send(`Account deleted!`);
+})
+
+//Read all bank accounts
+router.get('/',(req,res)=>{
+    console.log(`Get request of all accounts!`)
+    res.send(`Reading all accounts!`);
+})
+
+
+// //sanity
+// router.get('/test',(req,res)=>{
+//     res.send(`Loud and Clear`);
+// })
 
 //export route
 module.exports = router;
