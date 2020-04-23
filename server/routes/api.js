152aae18ac1471095c267dd1289c9c51e1e7ab03
MaryHooker <mc020791@yahoo.com>
2020-04-23 10:39:04 -0500

    endpoints stood up

--- server/routes/api.js
+++ server/routes/api.js
@@ -11,31 +11,46 @@ let BankAccountCollection = require('../models/BankAccountSchema');
 //Create new bank account
 router.post('/',(req,res)=>{
     console.log(`Create request!`)
-    res.send(`Account created!`);
+    // res.send(`Account created!`);
+    BankAccountCollection.create(req.body,(errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Read on bank account by account number
 router.get('/:accountNumber',(req,res)=>{
     console.log(`Get request of ${req.params.accountNumber}!`)
-    res.send(`Read specific account!`);
+    // res.send(`Read specific account!`);
+    BankAccountCollection.findOne({accountNumber:req.params.accountNumber},(errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
-//Update bank account balance
-router.put('/:accountBalance',(req,res)=>{
-    console.log(`Update request of ${req.params.accountBalance}!`)
-    res.send(`Account updated!`);
+//Update bank account 
+router.put('/:accountName',(req,res)=>{
+    console.log(`Update request of ${req.params.accountName}!`)
+    // res.send(`Account updated!`);
+    BankAccountCollection.findOneAndUpdate({accountName:req.params.accountName}, req.body, {results:true},(errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
-//Delete bank account balance
-router.delete('/:accountBalance',(req,res)=>{
-    console.log(`Delete request of ${req.params.accountBalance}!`)
-    res.send(`Account deleted!`);
+//Delete bank account
+router.delete('/:accountName',(req,res)=>{
+    console.log(`Delete request of ${req.params.accountName}!`)
+    // res.send(`Account deleted!`);
+    BankAccountCollection.findOneAndDelete({accountName:req.params.accountName}, (errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Read all bank accounts
 router.get('/',(req,res)=>{
     console.log(`Get request of all accounts!`)
-    res.send(`Reading all accounts!`);
+    // res.send(`Reading all accounts!`);
+    BankAccountCollection.find({},(errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 
