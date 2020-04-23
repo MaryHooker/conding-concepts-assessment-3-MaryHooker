aecd85d55aafd156a63e1ff876a1c1f7fec6e432
MaryHooker <mc020791@yahoo.com>
2020-04-23 11:18:53 -0500

    displaying available accounts

--- client/src/components/ListAccounts.jsx
+++ client/src/components/ListAccounts.jsx
@@ -3,12 +3,50 @@ import React,{Component} from 'react';
 class ListAccounts extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            availableAccounts:[],
+         }
     }
+
+    //lifecycle method to run inner function when component mounts
+    componentDidMount(){
+        this.loadData();
+    }
+
+    //Fetch all accounts from database
+    loadData = async() => {
+        //fetch from endpoint
+        let response = await fetch('/api');
+        //pull out json data
+        let json = await response.json();
+        //sanity
+        console.table(json);
+        //place data in stated array
+        this.setState(
+            {
+                availableAccounts:json
+            }
+        )
+
+    }
+
     render() { 
         return ( 
             <div>
-                <h2>List Accounts</h2>
+                <h2>Available Accounts</h2>
+                {
+                    this.state.availableAccounts.map((account)=>{
+                        return(
+                            <div key={account._id}>
+                                <p>Account #: {account.accountNumber}</p>
+                                <p>Type: {account.accountType}</p>
+                                <p>Name: {account.accountName}</p>
+                                <p>Balance: ${account.accountBalance}</p>
+                                <hr/>
+                            </div>
+                        )
+                    })
+                }
             </div>
          );
     }
